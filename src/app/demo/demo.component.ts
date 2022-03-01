26bb464ec5c6280922e7834b21afb81c57f9ed3a
Alexander Wiebe <alexanderjwiebe@gmail.com>
2022-03-01 11:23:30 -0600

    updated demo with notes

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'app-demo',
+  },
 })
 export class DemoComponent implements OnInit {
+  demoClicked: number = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  incDemo() {
+    this.demoClicked++;
   }
 
+  ngOnInit(): void {}
 }
